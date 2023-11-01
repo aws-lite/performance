0b35f6c35e0ca89406a3c8123f42b2a86e4dc6e6
Ryan Block <rblock@gmail.com>
2023-10-31 21:35:45 -0700

    Add benchmark for bundled `aws-sdk/clients/dynamodb` (SDK v2)

--- src/plugins/lambdas.mjs
+++ src/plugins/lambdas.mjs
@@ -8,7 +8,7 @@ const names = [
   'aws-lite-raw',
   'aws-lite-bundled',
   'aws-sdk-v2-raw',
-  // 'aws-sdk-v2-bundled', // Run on nodejs18.x
+  'aws-sdk-v2-bundled', // Run on nodejs18.x
   'aws-sdk-v3-raw',
   // 'aws-sdk-v3-bundled',
 ]
